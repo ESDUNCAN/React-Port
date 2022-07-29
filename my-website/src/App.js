a18dcdfed87cdb06a0dafdb2ba71b82b4ea4f95c
Eric Duncan <deric704@yahoo.ca>
2022-07-29 11:53:42 -0400

    created shared folder

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,18 +1,18 @@
-import {Routes, Route, Navigate } from "react-router-dom";
-import {useState,useEffect} from "react"
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "./shared/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
-    fetch('https://gitconnected.com/v1/portfolio/lancey1')
-      .then(res => res.json())
-      .then(user => {
-        console.log('success')
+    fetch("https://gitconnected.com/v1/portfolio/lancey1")
+      .then((res) => res.json())
+      .then((user) => {
+        console.log("success");
         setUser(user);
       });
   }, []);
@@ -22,13 +22,14 @@ export default function App() {
 
   return (
     <main>
-     <Navbar />
-     <Routes>
-        <Route exact path="/about" element={<About user={user}/>} />
+      <Navbar />
+      <Routes>
+        <Route exact path="/about" element={<About user={user} />} />
         {/* <Route exact path="/" element={<About user={user}/>} /> */}
-        <Route exact path="/" element={<Home user={user}/>} />
+        <Route exact path="/" element={<Home user={user} />} />
         <Route exact path="/projects" element={<Projects user={user} />} />
-        <Route path="*" element={<Navigate to="/about"/>} />
+        <Route path="*" element={<Navigate to="/about" />} />
       </Routes>
     </main>
   );
+}
