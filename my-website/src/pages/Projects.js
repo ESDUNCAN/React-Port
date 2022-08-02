b7e2fa42dc416039f78a7530a0e553a1ca465640
Eric Duncan <deric704@yahoo.ca>
2022-08-02 10:37:48 -0400

    created Deck and Card, edited to projects to be a slide show

--- my-website/src/pages/Projects.js
+++ my-website/src/pages/Projects.js
@@ -1,27 +1,9 @@
-import styles from "./Projects.module.css";
-import { Pill } from "../styles";
-import { ProjectItem, SkillContainer, ProjectTitle } from "../projectStyles";
-function Projects(props) {
-  const { user } = props;
+import Deck from "../components/Deck";
 
+function Projects(props) {
   return (
-    <div className={`${styles.container}`}>
-      <ul>
-        {user.projects.map((project, i) => (
-          <ProjectItem key={i}>
-            <ProjectTitle>{project.displayName}</ProjectTitle>
-            <p>{project.summary}</p>
-            <a href={`${project.website}`} target="_blank">
-              GitHub<i class="fa fa-github"></i>
-            </a>
-            <SkillContainer>
-              {[...project.languages, ...project.libraries].map((item, j) => (
-                <Pill key={j}>{item}</Pill>
-              ))}
-            </SkillContainer>
-          </ProjectItem>
-        ))}
-      </ul>
+    <div>
+      <Deck />
     </div>
   );
 }
