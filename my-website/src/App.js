1347318913cb205b7743599b30d84956b70793e4
Eric Duncan <deric704@yahoo.ca>
2022-08-04 13:29:30 -0400

    fixed projects and added logic to handle is there is no image

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Navbar from "./shared/Navbar";
-// import Projects from "./pages/Projects";
+import Projects from "./pages/Projects";
 import Home from "./pages/Home";
 import MyCat from "./pages/MyCat";
 import Work from "./pages/Work";
@@ -25,7 +25,7 @@ export default function App() {
     <main>
       <Navbar />
       <Routes>
-        {/* <Route exact path="/projects" element={<Projects user={user} />} /> */}
+        <Route exact path="/projects" element={<Projects user={user} />} />
         <Route exact path="/" element={<Home user={user} />} />
         <Route exact path="/mycat" element={<MyCat user={user} />} />
         <Route path="*" element={<Navigate to="/" />} />
---
+++ my-website/src/components/Project-Carousel.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import styles from "./Project-Carousel.module.css";
+import { useMediaQuery } from "react-responsive";
+import leah from "../images/leah.png";
+
+export default function ControlledCarousel(props) {
+  const [index, setIndex] = useState(0);
+
+  const isDesktop = useMediaQuery({ query: "(min-width: 1070px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 1069px)" });
+  console.log({ isTablet });
+  console.log({ isDesktop });
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  const projectList = props.projects.map((project) => {
+    console.log("PROJECTT IS HERE", project);
+
+    let image;
+    if (project.images.length > 0) {
+      image = project.images[1].resolutions.desktop.url;
+    } else {
+      image = leah;
+    }
+
+    return (
+      <Carousel.Item className={`${styles.carouselItem}`}>
+        {isDesktop && (
+          <a href={project.githubUrl} target="_blank" rel="noreferrer">
+            <img
+              className={`${styles.carouselImage}`}
+              src={image}
+              alt={project.displayName}
+            />
+          </a>
+        )}
+        {isTablet && (
+          <img
+            className={`${styles.carouselImage}`}
+            src={image}
+            alt={project.displayName}
+          />
+        )}
+        <div className={`${styles.carouselCaption}`}>
+          <a
+            href={project.githubUrl}
+            title={`to ${project.displayName} repo`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <b>{project.displayName}</b>
+          </a>
+          <p>{project.summary}</p>
+          <div>
+            <b>Tech Stack</b>
+          </div>
+          <div className={`${styles.carouselLi}`}>
+            {project.languages.map((language) => (
+              <p className={`${styles.carouselLis}`}>{language}</p>
+            ))}
+          </div>
+        </div>
+      </Carousel.Item>
+    );
+  });
+
+  return (
+    <div className={`${styles.container}`}>
+      <Carousel
+        variant="dark"
+        activeIndex={index}
+        onSelect={handleSelect}
+        interval={null}
+      >
+        {projectList}
+      </Carousel>
+    </div>
+  );
+}
